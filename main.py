3d12668a69629b6e17080f3d9ec85906ef87ac62
shilpigoelVB <98317653+shilpigoelVB@users.noreply.github.com>
2022-09-06 12:25:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     version = os.getenv('image_url', default = '0.0.0.1')
-    return {"Message": "With ❤️ from OCI DevOps via GitHubLab Actions","Version":version}
+    return {"Message": "With ❤️ from OCI DevOps via GitHub Actions","Version":version}
